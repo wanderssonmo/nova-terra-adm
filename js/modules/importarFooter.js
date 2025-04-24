fda9eb06f8a23f7e091bbff7d89f89e691f55b67
Wandersson <wandersonmowanfatec@gmail.com>
2025-04-23 21:46:18 -0300

    Atualizei footer e portal do cliente
    Atualização geral no footer e portal do cliente

--- js/modules/importarFooter.js
+++ js/modules/importarFooter.js
@@ -1,4 +1,26 @@
 
-fetch('footer.html')
-        .then(response => response.text())
-        .then(data => document.getElementById('footer').innerHTML = data);
+        fetch('footer.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('footer').innerHTML = data;
+
+    // Agora que o footer foi carregado, podemos ativar o botão
+    const btnTopo = document.getElementById("btn-topo");
+
+    // Mostrar ou ocultar o botão
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 200) {
+        btnTopo.style.display = "flex";
+      } else {
+        btnTopo.style.display = "none";
+      }
+    });
+
+    // Rolagem suave para o topo
+    btnTopo.addEventListener("click", () => {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    });
+  });
