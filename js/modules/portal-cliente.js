aaa08a079b24a6f9f0f8bbd746f48c7f409368ac
Wandersson <wandersonmowanfatec@gmail.com>
2025-05-02 22:51:08 -0300

    Footer e Portal do Cliente
    Atualizei os créditos  no footer e o botão  de redirecionar no portal do cliente.

--- js/modules/portal-cliente.js
+++ js/modules/portal-cliente.js
@@ -2,14 +2,27 @@ document.addEventListener("DOMContentLoaded", function () {
     const botaoPortal = document.getElementById("portal-button");
     const botaoPortal2 = document.getElementById("portal-button-2");
 
+    // Função para redirecionar com aviso
     const redirecionar = () => {
-        // Substitua pela URL correta do Portal do Cliente
-        const url = "https://iis.aws.sistemasivi.com.br:10704/incorporacaosolar/Account/Login";
-        window.open(url, "_blank"); // abre em nova aba/janela
-    };
+        Swal.fire({
+          title: 'Redirecionamento',
+          text: 'Você será redirecionado com segurança para o Portal do Cliente em uma nova aba. Deseja continuar?',
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonText: 'Sim, acessar',
+          cancelButtonText: 'Cancelar',
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            const url = "https://iis.aws.sistemasivi.com.br:10704/incorporacaosolar/Account/Login";
+            window.open(url, "_blank");
+          }
+        });
+      };
 
+    // Adiciona o evento ao botão se ele existir
     if (botaoPortal) {
         botaoPortal.addEventListener("click", redirecionar);
     }
-
 });
