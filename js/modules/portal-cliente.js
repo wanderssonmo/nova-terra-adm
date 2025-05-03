8ee678e912a584d3c8f6f4f32a56369b1b536497
Wandersson <wandersonmowanfatec@gmail.com>
2025-05-02 23:09:40 -0300

    Portal cliente botão redirecionar
    Atualizei o botão redirecionar no portal do cliente.

--- js/modules/portal-cliente.js
+++ js/modules/portal-cliente.js
@@ -1,28 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const botaoPortal = document.getElementById("portal-button");
+    const botaoPortal1 = document.getElementById("portal-button-1");
     const botaoPortal2 = document.getElementById("portal-button-2");
 
-    // Função para redirecionar com aviso
     const redirecionar = () => {
         Swal.fire({
-          title: 'Redirecionamento',
-          text: 'Você será redirecionado com segurança para o Portal do Cliente em uma nova aba. Deseja continuar?',
-          icon: 'warning',
-          showCancelButton: true,
-          confirmButtonText: 'Sim, acessar',
-          cancelButtonText: 'Cancelar',
-          confirmButtonColor: '#3085d6',
-          cancelButtonColor: '#d33'
+            title: 'Redirecionamento',
+            text: 'Você será redirecionado para o Portal do Cliente em uma nova aba. Deseja continuar?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Sim, acessar',
+            cancelButtonText: 'Cancelar',
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33'
         }).then((result) => {
-          if (result.isConfirmed) {
-            const url = "https://iis.aws.sistemasivi.com.br:10704/incorporacaosolar/Account/Login";
-            window.open(url, "_blank");
-          }
+            if (result.isConfirmed) {
+                const url = "https://iis.aws.sistemasivi.com.br:10704/incorporacaosolar/Account/Login";
+                window.open(url, "_blank");
+            }
         });
-      };
+    };
 
-    // Adiciona o evento ao botão se ele existir
-    if (botaoPortal) {
-        botaoPortal.addEventListener("click", redirecionar);
+    if (botaoPortal1) {
+        botaoPortal1.addEventListener("click", redirecionar);
+    }
+
+    if (botaoPortal2) {
+        botaoPortal2.addEventListener("click", redirecionar);
     }
 });
