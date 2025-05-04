f1f79b8d78e72151f451de2c0e97f883fb261ac1
Wandersson <wandersonmowanfatec@gmail.com>
2025-05-03 23:36:56 -0300

    Portal do Cliente, Imóveis e Footer
    Atualizei o redirecionamento do portal do cliente e mapas dos imóveis, também ajustei o CSS do footer colocando espaçamento proporcional.

--- js/modules/imoveis.js
+++ js/modules/imoveis.js
@@ -1,30 +1,38 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const botoesVerMapa = document.querySelectorAll('.btn--secondary');
-  
-    botoesVerMapa.forEach((botao) => {
-      botao.addEventListener("click", function (event) {
-        event.preventDefault(); // Impede qualquer comportamento padrão
-  
-        const link = botao.closest(".mapa-link").dataset.url;
-  
-        if (link && link !== "") {
-          Swal.fire({
-            title: 'Você será redirecionado',
-            text: 'Deseja visualizar o mapa deste imóvel em uma nova aba?',
-            icon: 'info',
-            showCancelButton: true,
-            confirmButtonText: 'Sim, abrir mapa',
-            cancelButtonText: 'Cancelar',
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33'
-          }).then((result) => {
-            if (result.isConfirmed) {
-              window.open(link, "_blank");
-            }
-          });
-        }
-      });
+  const botoesVerMapa = document.querySelectorAll('.btn--secondary');
+
+  botoesVerMapa.forEach((botao) => {
+    botao.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      const link = botao.closest(".mapa-link").dataset.url;
+
+      if (link && link !== "") {
+        Swal.fire({
+          title: 'Redirecionamento',
+          text: 'Abrir de forma segura o mapa do imóvel em nova aba?',
+          icon: 'info',
+          showCancelButton: true,
+          confirmButtonText: 'Sim',
+          cancelButtonText: 'Cancelar',
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          customClass: {
+            popup: 'swal2-custom-popup',
+            title: 'swal2-custom-title',
+            htmlContainer: 'swal2-custom-text',
+            confirmButton: 'swal2-custom-confirm',
+            cancelButton: 'swal2-custom-cancel'
+          }
+        }).then((result) => {
+          if (result.isConfirmed) {
+            window.open(link, "_blank");
+          }
+        });
+      }
     });
   });
+});
+
   
   
--- js/modules/portal-cliente.js
+++ js/modules/portal-cliente.js
@@ -1,30 +1,39 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const botaoPortal1 = document.getElementById("portal-button-1");
+    const botaoPortal = document.getElementById("portal-button-1");
     const botaoPortal2 = document.getElementById("portal-button-2");
-
-    const redirecionar = () => {
-        Swal.fire({
-            title: 'Redirecionamento',
-            text: 'Você será redirecionado para o Portal do Cliente em uma nova aba. Deseja continuar?',
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonText: 'Sim, acessar',
-            cancelButtonText: 'Cancelar',
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33'
-        }).then((result) => {
-            if (result.isConfirmed) {
-                const url = "https://iis.aws.sistemasivi.com.br:10704/incorporacaosolar/Account/Login";
-                window.open(url, "_blank");
-            }
-        });
+  
+    const redirecionarComAviso = () => {
+      const url = "https://iis.aws.sistemasivi.com.br:10704/incorporacaosolar/Account/Login";
+  
+      Swal.fire({
+        title: 'Redirecionamento',
+        text: 'Você será direcionado com segurança para o Portal do Cliente em uma nova aba.',
+        icon: 'info',
+        showCancelButton: true,
+        confirmButtonText: 'Continuar',
+        cancelButtonText: 'Cancelar',
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        customClass: {
+          popup: 'swal2-custom-popup',
+          title: 'swal2-custom-title',
+          htmlContainer: 'swal2-custom-text',
+          confirmButton: 'swal2-custom-confirm',
+          cancelButton: 'swal2-custom-cancel'
+        }
+      }).then((result) => {
+        if (result.isConfirmed) {
+          window.open(url, "_blank");
+        }
+      });
     };
-
-    if (botaoPortal1) {
-        botaoPortal1.addEventListener("click", redirecionar);
+  
+    if (botaoPortal) {
+      botaoPortal.addEventListener("click", redirecionarComAviso);
     }
-
+  
     if (botaoPortal2) {
-        botaoPortal2.addEventListener("click", redirecionar);
+      botaoPortal2.addEventListener("click", redirecionarComAviso);
     }
-});
+  });
+  
